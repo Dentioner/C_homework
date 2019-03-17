f2349cacaf73b541b193b4ab93ac2a96cb5af0bb
Dentioner <we980127@gmail.com>
2019-03-17 20:12:41 +0800

    hw on OJ

--- 数据结构/3.21/3.21.c
+++ 数据结构/3.21/3.21.c
@@ -13,26 +13,41 @@
 #define STACK_SIZE_INIT 100
 #define STACK_SIZE_ADD 10
 #define BRACKET_PRIORITY 10
+#define ADD '+'
+#define SUB '-'
+#define MUL '*'
+#define DIV '/'
 
+
+typedef struct element
+{
+	int symbol;
+	int priority;
+}ele;
 
 typedef struct stack
 {
-	int* top;
-	int* bottom;
+	ele* top;
+	ele* bottom;
 	int size;
 }stack;
+
+
+
 
 void push(stack*s, int ele);
 void pop(stack*s);
 int this_is_not_operator(int ch);
-int compare_priority(int ch);
+void compare_priority(int ch);
+int get_priority(int ch);
+
 int left_bracket = 0;//表示出现左括号的数量，特别是出来好几个左括号的时候
 
 stack sheet;
 
 int main()
 {
-    sheet.bottom = (int*)malloc(STACK_SIZE_INIT * sizeof(int));
+    sheet.bottom = (ele*)malloc(STACK_SIZE_INIT * sizeof(ele));
     if (sheet.bottom == NULL)
 		exit(1);
 	sheet.top = sheet.bottom ;
@@ -52,15 +67,20 @@ int main()
         {
             if (ch == '(')
                 left_bracket++;
-            else if (ch == ')')
-                left_bracket--;
-            else
+			else if (ch == ')')
+			{
+				left_bracket--;
+				if (left_bracket < 0)
+					exit(1);
+			}
+			else
             {
-                 if (sheet.bottom == sheet.top)//如果栈是空的，直接入栈
-                push(&sheet, ch);
+                if (sheet.bottom == sheet.top)//如果栈是空的，直接入栈
+					push(&sheet, ch);
                 else
                 {
-                    //写到这里了
+					compare_priority(ch);
+
                 } 
             }
             
@@ -69,6 +89,13 @@ int main()
         
     }
 
+
+	while (sheet.bottom != sheet.top)
+	{
+		ch = (*(sheet.top - 1)).symbol;
+		printf("%c", ch);
+		sheet.top--;
+	}
 
     system("pause");
     return 0;
@@ -86,7 +113,9 @@ void push(stack*s, int ele)
 		s->top = s->bottom + s->size;
 		s->size += STACK_SIZE_ADD;
 	}
-	*(s->top++) = ele;
+	(*(s->top)).symbol = ele;
+	(*(s->top)).priority = get_priority(ele);
+	s->top++;
 }
 
 void pop(stack*s)
@@ -113,8 +142,34 @@ int this_is_not_operator(int ch)
 }
 
 
-int compare_priority(int ch)
+void compare_priority(int input_ch)
 {
-    int a;
+	int pr_a, pr_ch;
+	int a = (*(sheet.top - 1)).symbol;
+	pr_a = (*(sheet.top - 1)).priority;
+	pr_ch = get_priority(input_ch);
+	if (pr_a >= pr_ch)
+	{
+		printf("%c", a);
+		pop(&sheet);
+		compare_priority(input_ch);
+	}
+	else
+	{
+		push(&sheet, input_ch);
 
+	}
+	
+	return;
+}
+
+int get_priority(int ch)
+{
+	int pr = 0;
+	if (ch == ADD || ch == SUB)
+		pr = 1;
+	else if (ch == MUL || ch == DIV)
+		pr = 5;
+	pr = pr + 10 * left_bracket;
+	return pr;
 }
