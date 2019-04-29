cb1d71c1ef4d2ff65bae4bafd309031d538309ac
Dentioner <842007656@qq.com>
2019-04-29 17:53:25 +0900

    update prj1

--- DataStructure/project/1/main.c
+++ DataStructure/project/1/main.c
@@ -1,11 +1,33 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+
+int board[8][8];
+int location[2];
+int step_counter;
+int vector[8][2] = {{-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}, {-2, 1}};
+
+void printboard()
+{
+    for (int i = 0; i<8;i++)
+    {
+        for (int j  = 0; j<8;j++)
+        {
+            printf("%-4d", board[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void next_step()
+{
+    
+}
+
 int main()
 {
-    printf("©°©¤©´\n");
-    printf("©¦1©¦\n");
-    printf("©¸©¤©¼\n");
+    printboard();
     system("pause");
     return 0;
 }
+
