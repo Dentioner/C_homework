46298fabe809c09e7d5cd2132c1efccd985adefe
Dentioner Tchang <we980127@gmail.com>
2019-08-31 22:28:33 +0800

    update

--- OperatingSystem/addr_space.c
+++ OperatingSystem/addr_space.c
@@ -7,9 +7,14 @@ char bdata[16] = {1,2,3,4};
 main() {
 	char * ldata[16];	
 	char * ddata;
-
+	
 	ddata = malloc(16);
+	system("cat /proc/self/maps");
+	printf("\n\n");
 	printf("gdata: %llX\nbdata:%llX\nldata:%llx\nddata:%llx\n",
 		gdata,bdata,ldata,ddata);
+	system("cat /proc/self/maps");
 	free(ddata);
+	printf("\n\n");
+	system("cat /proc/self/maps");
 }
