ef6475eaeb86875e6a4da060ea7c5d23e5fad143
Dentioner <we980127@gmail.com>
2019-09-14 11:39:00 +0800

    prj

---
+++ OS_experiment/prj1/bonus/bootblock.s
@@ -0,0 +1,62 @@
+.text
+.global main
+
+main:
+	# 1) task1 call BIOS print string "It's bootblock!"
+	# la	$a0, msg
+	# la	$t0, printstr
+	# lw	$t1, 0($t0)
+	
+	la	$a0, msg
+	lw	$t1, (printstr)
+	jalr	$t1
+	nop
+
+	# la	$t0, kernel
+	# lw	$a
+	# read the size of kernel from the first sector
+	lw	$t3, (kernel)
+	lb	$t4, -3($t3)
+	li	$t5, 0x200
+	
+	lw	$a0, (kernel)
+	li	$a1, 0x200
+	li	$a2, 0x400
+	lw	$t0, (read_sd_card)
+
+
+	lw	$ra, (kernel_main)
+	jr	$t0	
+#	lw	$ra, kernel_main
+#	j	0x80011000	
+	
+	# 2) task2 call BIOS read kernel in SD card and jump to kernel start
+	
+# while(1) --> stop here
+stop:
+	j stop
+
+.data
+
+msg: .ascii "It's a bootloader...\n"
+# info:	.ascii "Hello OS!\n"
+
+# 1. PMON read SD card function address
+# read_sd_card();
+read_sd_card: .word 0x80011000
+
+# 2. PMON print string function address
+# printstr(char *string)
+printstr: .word 0x80011100
+
+# 3. PMON print char function address
+# printch(char ch)
+printch: .word 0x80011140
+
+# 4. kernel address (move kernel to here ~)
+# kernel : .word 0xa0800200
+kernel : .word 0xa0800000
+
+# 5. kernel main address (jmp here to start kernel main!)
+# kernel_main : .word 0xa0800200
+kernel_main : .word 0xa0800000
---
+++ OS_experiment/prj1/bonus/kernel.c
@@ -0,0 +1,55 @@
+char ch[14] = "Version:0000\n";
+int value = 2019;
+
+void __attribute__((section(".entry_function"))) _start(void)
+{
+	// Call PMON BIOS printstr to print message "Hello OS!"
+	void (*printstr)(char *) = (void*)0x80011100;
+	int res = 0;
+	printstr("Hello OS! ");
+	if(value >= 1000)
+	{
+			res = value / 1000;
+			ch[8] += res;
+			value -= res*1000;
+	}
+
+	if(value >= 100)
+	{
+			res = value / 100;
+			ch[9] += res;
+			value -= res*100;   
+	}
+
+	if (value >= 10)
+	{
+			res = value/10;
+			ch[10] += res;
+			value -= res*10;
+	}
+
+	if (value >= 1)
+	{
+			ch[11] += value;
+	}
+
+	printstr(ch);
+	
+	while(1)
+		;
+
+	return;
+
+/*	char* info= "Hello OS!\n";
+	asm(
+		"la	$a0, %0\n\t"
+		"lw	$t2, 0x80011100\n\t"
+		"jalr	$t2\n\t"
+		"nop	\n\t"
+		:
+		:"r"(info)
+		:"a0", "t2");
+*/
+	//printf("Hello OS!\n");
+	//return;
+}
