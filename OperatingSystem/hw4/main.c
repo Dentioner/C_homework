d0e0d61bcc11418b3a0308a301e3384548bac5d1
Dentioner <we980127@gmail.com>
2019-09-18 13:38:40 +0800

    update

---
+++ OperatingSystem/hw4/main.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<sys/types.h>
+
+
+int  main()
+{
+     pid_t  pid;
+     int  i;
+     for  (i=0;  i<3;  i++)
+      {
+           /* fork  another  process  */
+           pid = fork();
+           if  (pid < 0) { /*error  occurred  */
+                fprintf(stderr, "Fork Failed");
+                exit(-1);
+           }
+           else if (pid == 0) { /* child process */
+                fprintf(stdout,  "i=%d,  pid=%d,  parent  pid=%d\n",i,      
+                getpid() ,getppid());
+           }   
+      }
+      wait(NULL);
+      exit(0);
+} 
