a62b164060f2cddff9df54dad20cc4b0b4ec509f
Dentioner <we980127@gmail.com>
2019-10-27 16:54:02 +0800

    prj3 part1 finished(maybe...)

---
+++ OS_experiment/prj3/step1&2/start_code/include/sys/syscall.h
@@ -0,0 +1,90 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * * * * *
+ *            Copyright (C) 2018 Institute of Computing Technology, CAS
+ *               Author : Han Shukai (email : hanshukai@ict.ac.cn)
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * * * * *
+ *                       System call related processing
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * * * * *
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE. 
+ * 
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *  * * * * * * * * * * */
+
+#ifndef INCLUDE_SYSCALL_H_
+#define INCLUDE_SYSCALL_H_
+
+#include "type.h"
+#include "sync.h"
+#include "queue.h"
+#include "sched.h"
+
+#define IGNORE 0
+#define NUM_SYSCALLS 64
+
+/* define */
+#define SYSCALL_SLEEP 2
+
+#define SYSCALL_PS 3
+#define SYSCALL_SPAWN 4
+#define SYSCALL_KILL 5
+#define SYSCALL_EXIT 6
+#define SYSCALL_WAIT 7
+
+#define SYSCALL_BLOCK 10
+#define SYSCALL_UNBLOCK_ONE 11
+#define SYSCALL_UNBLOCK_ALL 12
+
+#define SYSCALL_WRITE 20
+#define SYSCALL_READ 21
+#define SYSCALL_CURSOR 22
+#define SYSCALL_REFLUSH 23
+#define SYSCALL_CLEAR 24
+
+#define SYSCALL_MUTEX_LOCK_INIT 30
+#define SYSCALL_MUTEX_LOCK_ACQUIRE 31
+#define SYSCALL_MUTEX_LOCK_RELEASE 32
+
+
+
+/* syscall function pointer */
+int (*syscall[NUM_SYSCALLS])(); // this is an array of function pointer
+
+void system_call_helper(int, int, int, int);
+extern int invoke_syscall(int, int, int, int);
+
+void sys_sleep(uint32_t);
+void sys_process_show();
+void sys_spawn(task_info_t * task);
+void sys_kill(pid_t pid);
+void sys_exit();
+void sys_waitpid(pid_t pid);
+
+void sys_block(queue_t *);
+void sys_unblock_one(queue_t *);
+void sys_unblock_all(queue_t *);
+
+void sys_write(char *);
+void sys_move_cursor(int, int);
+void sys_reflush();
+void sys_clear(int line1, int line2);
+
+void mutex_lock_init(mutex_lock_t *);
+void mutex_lock_acquire(mutex_lock_t *);
+void mutex_lock_release(mutex_lock_t *, pcb_t *);
+
+#endif
---
+++ OS_experiment/prj3/step1&2/start_code/kernel/locking/lock.c
@@ -0,0 +1,86 @@
+#include "lock.h"
+#include "sched.h"
+#include "syscall.h"
+
+
+//extern queue_t ready_queue;
+
+//extern pcb_t* current_running;
+extern mutex_lock_t lock1;
+extern mutex_lock_t lock2;
+void spin_lock_init(spin_lock_t *lock)
+{
+    lock->status = UNLOCKED;
+}
+
+void spin_lock_acquire(spin_lock_t *lock)
+{
+    while (LOCKED == lock->status)
+    {
+    };
+    lock->status = LOCKED;
+}
+
+void spin_lock_release(spin_lock_t *lock)
+{
+    lock->status = UNLOCKED;
+}
+
+void do_mutex_lock_init(mutex_lock_t *lock)
+{
+    lock->status = UNLOCKED;
+    queue_init(&(lock->block_queue));
+    lock->locked_process_pid = 0;
+    lock->address_of_locked_process = NULL;
+}
+
+
+void do_mutex_lock_acquire(mutex_lock_t *lock)
+{
+
+    current_running->target_lock_id[lock->lock_self_id] = WANT_LOCK;
+    if(lock->status == UNLOCKED)
+    {
+        lock->status = LOCKED;
+        lock->locked_process_pid = current_running->pid;
+        lock->address_of_locked_process = (uint32_t)current_running;
+        //current_running->lock_address = lock;
+        current_running->target_lock_id[lock->lock_self_id] = GET_LOCK;
+    }
+    else
+    {
+        /* code */
+        int index1;
+        if(current_running->target_lock_id[0] == GET_LOCK)
+            do_mutex_lock_release(&lock1, current_running);
+        if(current_running->target_lock_id[1] == GET_LOCK)
+            do_mutex_lock_release(&lock2, current_running);
+
+        current_running->target_lock_id[lock->lock_self_id] = NOT_GET_LOCK;
+        do_block(&(lock->block_queue));
+    }
+    
+}
+
+
+
+void do_mutex_lock_release(mutex_lock_t *lock, pcb_t * process)
+{// this function just release one process out of the array.
+
+    //pcb_t *tmp = &pcb[index_of_pcb];
+    lock->address_of_locked_process = NULL;
+    lock->locked_process_pid = 0;
+
+    lock->status = UNLOCKED;
+
+    do_unblock_one(&lock->block_queue);
+
+    if(process->target_lock_id[lock->lock_self_id] == GET_LOCK)
+        process->target_lock_id[lock->lock_self_id] = WANT_LOCK;
+    else
+    {
+        printk("error condition of lock.\n");
+        while(1);
+    }
+    
+}
