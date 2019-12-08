dbb1f453f1c1e7bf326985b48826cf2a2fc4398d
Dentioner <we980127@gmail.com>
2019-12-08 16:26:27 +0800

    update prj5

---
+++ OS_experiment/prj5/step1/start_code/drivers/mac.c
@@ -0,0 +1,283 @@
+#include "mac.h"
+#include "irq.h"
+
+
+
+#define NUM_DMA_DESC 48
+queue_t recv_block_queue;
+uint32_t recv_flag[PNUM] = {0};
+uint32_t ch_flag;
+uint32_t mac_cnt = 0;
+uint32_t reg_read_32(uint32_t addr)
+{
+    return *((uint32_t *)addr);
+}
+uint32_t read_register(uint32_t base, uint32_t offset)
+{
+    uint32_t addr = base + offset;
+    uint32_t data;
+
+    data = *(volatile uint32_t *)addr;
+    return data;
+}
+
+void reg_write_32(uint32_t addr, uint32_t data)
+{
+    *((uint32_t *)addr) = data;
+}
+
+static void gmac_get_mac_addr(uint8_t *mac_addr)
+{
+    uint32_t addr;
+
+    addr = read_register(GMAC_BASE_ADDR, GmacAddr0Low);
+    mac_addr[0] = (addr >> 0) & 0x000000FF;
+    mac_addr[1] = (addr >> 8) & 0x000000FF;
+    mac_addr[2] = (addr >> 16) & 0x000000FF;
+    mac_addr[3] = (addr >> 24) & 0x000000FF;
+
+    addr = read_register(GMAC_BASE_ADDR, GmacAddr0High);
+    mac_addr[4] = (addr >> 0) & 0x000000FF;
+    mac_addr[5] = (addr >> 8) & 0x000000FF;
+}
+
+void print_tx_dscrb(mac_t *mac)
+{
+    uint32_t i;
+    printf("send buffer mac->saddr=0x%x ", mac->saddr);
+    printf("mac->saddr_phy=0x%x ", mac->saddr_phy);
+    printf("send discrb mac->td_phy=0x%x\n", mac->td_phy);
+#if 0
+    desc_t *send=mac->td;
+    for(i=0;i<mac->pnum;i++)
+    {
+        printf("send[%d].tdes0=0x%x ",i,send[i].tdes0);
+        printf("send[%d].tdes1=0x%x ",i,send[i].tdes1);
+        printf("send[%d].tdes2=0x%x ",i,send[i].tdes2);
+        printf("send[%d].tdes3=0x%x ",i,send[i].tdes3);
+    }
+#endif
+}
+
+void print_rx_dscrb(mac_t *mac)
+{
+    uint32_t i;
+    printf("recieve buffer add mac->daddr=0x%x ", mac->daddr);
+    printf("mac->daddr_phy=0x%x ", mac->daddr_phy);
+    printf("recieve discrb add mac->rd_phy=0x%x\n", mac->rd_phy);
+    desc_t *recieve = (desc_t *)mac->rd;
+#if 0
+    for(i=0;i<mac->pnum;i++)
+    {
+        printf("recieve[%d].tdes0=0x%x ",i,recieve[i].tdes0);
+        printf("recieve[%d].tdes1=0x%x ",i,recieve[i].tdes1);
+        printf("recieve[%d].tdes2=0x%x ",i,recieve[i].tdes2);
+        printf("recieve[%d].tdes3=0x%x\n",i,recieve[i].tdes3);
+    }
+#endif
+}
+
+static uint32_t printf_recv_buffer(uint32_t recv_buffer)
+{
+}
+void mac_irq_handle(void)
+{
+}
+
+void irq_enable(int IRQn)
+{
+}
+
+void mac_recv_handle(mac_t *test_mac)
+{
+}
+
+static uint32_t printk_recv_buffer(uint32_t recv_buffer)
+{
+}
+
+void set_sram_ctr()
+{
+    *((volatile unsigned int *)0xbfd00420) = 0x8000; /* 使能GMAC0 */
+}
+static void s_reset(mac_t *mac) //reset mac regs
+{
+    uint32_t time = 1000000;
+    reg_write_32(mac->dma_addr, 0x01);
+
+    while ((reg_read_32(mac->dma_addr) & 0x01))
+    {
+        reg_write_32(mac->dma_addr, 0x01);
+        while (time)
+        {
+            time--;
+        }
+    };
+}
+void disable_interrupt_all(mac_t *mac)
+{
+    reg_write_32(mac->dma_addr + DmaInterrupt, DmaIntDisable);
+    return;
+}
+void set_mac_addr(mac_t *mac)
+{
+    uint32_t data;
+    uint8_t MacAddr[6] = {0x00, 0x55, 0x7b, 0xb5, 0x7d, 0xf7};
+    uint32_t MacHigh = 0x40, MacLow = 0x44;
+    data = (MacAddr[5] << 8) | MacAddr[4];
+    reg_write_32(mac->mac_addr + MacHigh, data);
+    data = (MacAddr[3] << 24) | (MacAddr[2] << 16) | (MacAddr[1] << 8) | MacAddr[0];
+    reg_write_32(mac->mac_addr + MacLow, data);
+}
+uint32_t do_net_recv(uint32_t rd, uint32_t rd_phy, uint32_t daddr)
+{
+    int index1, index2, index3, index4;
+    desc_t * tmp_recv = (desc_t *)rd;
+    int recv_num = PNUM;
+    uint32_t * tmp_data;
+    
+
+    reg_write_32(DMA_BASE_ADDR + 0xc, rd_phy);
+    reg_write_32(GMAC_BASE_ADDR, reg_read_32(GMAC_BASE_ADDR) | 0x4); // let mac[3] = 1
+    reg_write_32(DMA_BASE_ADDR + 0x18, reg_read_32(DMA_BASE_ADDR + 0x18) | 0x02200002); // start tx, rx
+    // 0x02200002 = 0000_0010_0010_0000_0000_0000_0000_0010
+    // reg[25] = 1----> receive store and forward;
+    // reg[21] = 1----> Transmit store and forward;
+    // reg[1]  = 1----> Start receive;
+    reg_write_32(DMA_BASE_ADDR + 0x1c, 0x10001 | (1 << 6));
+    // 0x10001 | (1 << 6) = 0000_0000_0000_0001_0000_0000_0100_0001
+    // reg[16] = 1----> normal interrupt summary enable; 
+    // reg[6]  = 1----> receive interrupt enable;
+    // reg[0]  = 1----> transmit interrupt enable;
+
+
+    tmp_recv = (desc_t *)rd;
+    for(index1 = 0; index1 < PNUM; index1++)
+    {
+        //rx_desc_list[index1].tdes0 |= DESC_OWN;
+        //tmp_recv[index1].tdes0 |= DESC_OWN;
+        tmp_recv->tdes0 |= DESC_OWN;
+        tmp_recv = (desc_t *)((tmp_recv->tdes3) | GET_UNMAPPED_VADDR);
+    }
+
+    
+    for (index2 = 0; index2 < recv_num; index2++)
+    {
+        reg_write_32(DMA_BASE_ADDR + DmaRPD, 1);
+    }
+
+
+    tmp_recv = (desc_t *)rd;
+    index2 = 0;
+    index3 = 0;
+    while(1) // 轮询
+    {
+        if(!(tmp_recv->tdes0 & DESC_OWN)) // OWN = 0, received a package
+        {
+            if(tmp_recv->tdes0 & RECV_ERROR)
+            {
+                printk("RECV ERROR.\n");
+                while(1);
+            }
+            
+            vt100_move_cursor(1, 3);
+            printk("%d recv buffer, r_desc = 0x%x:\n", index3, tmp_recv->tdes0);
+            
+            tmp_data = (uint32_t *)(daddr + index2*0x400);
+            if((tmp_data[0]!=0xb57b5500) || (tmp_data[1]!= 0x5a70f77d) || (tmp_data[2]!= 0x0e42d30f)) // this means the package is not from pktRxTx to our kernel
+            {
+                // re-fetch another package
+                tmp_recv->tdes0 |= DESC_OWN;
+            }
+            else
+            {
+                for(index4 = 0; index4 < PSIZE; index4++)
+                {
+                    printk("%x  ", tmp_data[index4]);
+                }
+
+                index3++;
+            }
+        }
+
+        if(index3 >= recv_num) // received all the packages
+        {
+            break;
+        }
+        else
+        {
+            tmp_recv = (desc_t *)((tmp_recv->tdes3) | GET_UNMAPPED_VADDR);
+            index2 = (index2 + 1)%PNUM;
+        }
+        
+    }
+
+    vt100_move_cursor(1, 2);
+    printk("recv valid %d packages!:", index3);
+
+    return 0;
+}
+
+void do_net_send(uint32_t td, uint32_t td_phy)
+{
+    int index1, index2, index3, index4;
+    desc_t * tmp_send;
+    int send_num = PNUM;
+
+    reg_write_32(DMA_BASE_ADDR + 0x10, td_phy);
+
+    // MAC rx/tx enable
+
+    reg_write_32(GMAC_BASE_ADDR, reg_read_32(GMAC_BASE_ADDR) | 0x8);                    // enable MAC-TX
+    reg_write_32(DMA_BASE_ADDR + 0x18, reg_read_32(DMA_BASE_ADDR + 0x18) | 0x02202000); //0x02202002); // start tx, rx
+    // 0x02202000 = 0000_0010_0010_0000_0010_0000_0000_0000
+    // reg[25] = 1----> receive store and forward;
+    // reg[21] = 1----> Transmit store and forward;
+    // reg[13] = 1----> Start transmission;
+    
+
+
+    reg_write_32(DMA_BASE_ADDR + 0x1c, 0x10001 | (1 << 6));
+    // 0x10001 | (1 << 6) = 0000_0000_0000_0001_0000_0000_0100_0001
+    // reg[16] = 1----> normal interrupt summary enable; 
+    // reg[6]  = 1----> receive interrupt enable;
+    // reg[0]  = 1----> transmit interrupt enable;
+
+
+    tmp_send = (desc_t *)td;
+
+    for(index1 = 0; index1 < PNUM; index1++)
+    {
+        //tx_desc_list[index1].tdes0 |= DESC_OWN;
+        tmp_send->tdes0 |= DESC_OWN;
+        tmp_send = (desc_t *)((tmp_send->tdes3) | GET_UNMAPPED_VADDR);
+    }
+
+    for (index2 = 0; index2 < send_num; index2++)
+    {
+
+        reg_write_32(DMA_BASE_ADDR + DmaTPD, 1);
+    }
+
+}
+
+void do_init_mac(void)
+{
+    mac_t test_mac;
+    uint32_t i;
+
+    test_mac.mac_addr = 0xbfe10000;
+    test_mac.dma_addr = 0xbfe11000;
+
+    test_mac.psize = PSIZE * 4; // 64bytes
+    test_mac.pnum = PNUM;       // pnum
+
+    set_sram_ctr(); /* 使能GMAC0 */
+    s_reset(&test_mac);
+    disable_interrupt_all(&test_mac);
+    set_mac_addr(&test_mac);
+}
+
+void do_wait_recv_package(void)
+{
+}
