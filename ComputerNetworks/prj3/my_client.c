6b304bceec885acb088e5504b16286a1354fc001
Dentioner <we980127@gmail.com>
2020-04-20 18:16:25 +0800

    update

--- ComputerNetworks/prj3/my_client.c
+++ ComputerNetworks/prj3/my_client.c
@@ -84,49 +84,62 @@ int main()
             //printf("server reply : ");
             //printf("%s\n", server_reply);
 
-            for(index1 = 3; index1 < len; index1++)
+            //检查HTTP状态码是不是200 OK
+            if((server_reply[9] == '4') && (server_reply[10] == '0') && (server_reply[11] == '4'))
             {
-                if (server_reply[index1 - 3] == '\r' && server_reply[index1 - 2] == '\n' && server_reply[index1 - 1] == '\r' && server_reply[index1] == '\n')
+                printf("404 Not Found.\n");
+            }
+            else if((server_reply[9] == '2') && (server_reply[10] == '0') && (server_reply[11] == '0'))
+            {
+
+                for(index1 = 3; index1 < len; index1++)
                 {
-                    break;
+                    if (server_reply[index1 - 3] == '\r' && server_reply[index1 - 2] == '\n' && server_reply[index1 - 1] == '\r' && server_reply[index1] == '\n')
+                    {
+                        break;
+                    }
                 }
+
+                if(index1 >= len)
+                {
+                    perror("recv failed\n");
+                    return -1;
+                }
+
+                index1++;
+
+                //write data
+                char * p1 = strstr(server_reply, "Content-Length: ");
+                char a1[20];
+                memset(a1, 0, sizeof(a1));
+                p1 += 16;
+
+                int index5 = 0;
+                while(*p1 != '\r')
+                {
+                    a1[index5] = *p1;
+                    index5++;
+                    p1++;
+                }            
+
+                int truelen = atoi(a1);
+
+                time_tmp = time(NULL);
+                sprintf(file_name_get, "%ld", ((long)time_tmp % sizeof(file_name_get)));
+                f_get = fopen(file_name_get, "w");
+                fwrite((server_reply + index1), truelen, 1, f_get);
+                fclose(f_get);
+
+                printf("recv succeeded\n");
+
+                //debug
+                //printf("len=%d, true_len = %d\n",len, truelen);
             }
-
-            if(index1 >= len)
+            else
             {
-                perror("recv failed\n");
-                return -1;
+                printf("undefined status.\n");
             }
-
-            index1++;
-
-            //write data
-            char * p1 = strstr(server_reply, "Content-Length: ");
-            char a1[20];
-            memset(a1, 0, sizeof(a1));
-            p1 += 16;
-
-            int index5 = 0;
-            while(*p1 != '\r')
-            {
-                a1[index5] = *p1;
-                index5++;
-                p1++;
-            }            
-
-            int truelen = atoi(a1);
-
-            time_tmp = time(NULL);
-            sprintf(file_name_get, "%ld", ((long)time_tmp % sizeof(file_name_get)));
-            f_get = fopen(file_name_get, "w");
-            fwrite((server_reply + index1), truelen, 1, f_get);
-            fclose(f_get);
-
-            printf("recv succeeded\n");
-
-            //debug
-            printf("len=%d, true_len = %d\n",len, truelen);
-
+            
         }
 
     }
